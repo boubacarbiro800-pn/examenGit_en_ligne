075d8ef540eb083e3b40e77e5c210df33a453c43
boubacarbiro800-pn <boubacarbiro800@gmail.com>
2026-05-06 18:53:46 +0000

    Ajout des 10 fonctions

--- mesfonctions.py
+++ mesfonctions.py
@@ -1,1 +1,30 @@
 # premiere ligne
+def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b if b != 0 else None
+
+def carre(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+def est_pair(n):
+    return n % 2 == 0
+
+def est_impair(n):
+    return n % 2 != 0
+
+def max_val(a, b):
+    return max(a, b)
+
+def min_val(a, b):
+    return min(a, b)
